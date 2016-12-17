8880f36db49dc78010edb3290a5470a38021853c
rami <ramisaabmail@gmail.com>
2016-12-17 15:29:31 -0500

    Recursive URL parsing implemented
    Crawler can now handle nested tables

--- link_crawling_script.py
+++ link_crawling_script.py
@@ -1,46 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='https://www.epa.gov/enviro/sems-model'
 
+url_list=['https://www.epa.gov/enviro/frs-physical-data-model']
+for url in url_list:
+	# Get information for database URL
+	text=requests.get(url).text
+	soup=BeautifulSoup(text,'lxml')	
+	areas=soup.findAll('area')
+	areas
 
-text=requests.get(url).text
-soup=BeautifulSoup(text,'lxml')
+	# Find table names
+	table_names = [a.attrs['alt'] for a in areas]
+	table_links = [a.attrs['href'] for a in areas]
 
-areas=soup.findAll('area')
-areas
+	data_dic= {}
+	table_name_index = 0
 
-table_names = [a.attrs['alt'] for a in areas]
-#print(table_names)
-table_links = [a.attrs['href'] for a in areas]
-#print(table_links)
+	# Find columns in all table names
+	iteration = 0
+	for table_link in table_links:
+		columns_on_page = table_link.find("p_table_name=")
+		if (columns_on_page != -1):
+			print(iteration)
+			iteration+=1
+			text=requests.get(table_link).text
+			data_dic[table_names[table_name_index]] = {}
 
-data_dic= {}
+			soup=BeautifulSoup(text,'lxml')
+			links_list = soup.findAll('a')
+			links_list = [links.attrs['href'] for links in links_list]
 
-table_name_index = 0
-for table_link in table_links:
-	text=requests.get(table_link).text
-	data_dic[table_names[table_name_index]] = {}
+			column_link_list = [link for link in links_list if (link.find("/enviro/EF_METADATA_HTML") != -1)]
+			column_names = [None]*len(column_link_list)
+			
+			i = 0
+			for column in column_link_list:
+				start_column_index = column.find("p_column_name") + 14
+				end_column_index = column.find("&p_table_name")
+				column_name = column[start_column_index:end_column_index]
+				column_names[i] = column_name
+				i+=1
+			data_dic[table_names[table_name_index]]["columns"] = column_names
+			table_name_index += 1
+		else:
+			if (table_link.find("http") != -1):
+				url_list.append(table_link)
+			else:
+				url_list.append("https://www.epa.gov"+table_link)
 
-	soup=BeautifulSoup(text,'lxml')
-	links_list = soup.findAll('a')
-	links_list = [links.attrs['href'] for links in links_list]
-
-	column_link_list = [link for link in links_list if (link[0:23] == "//oaspub.epa.gov/enviro")]
-	#print(column_link_list)
-	column_names = [None]*len(column_link_list)
-	i = 0
-
-	for column in column_link_list:
-		start_column_index = column.find("p_column_name") + 14
-		end_column_index = column.find("&p_table_name")
-		column_name = column[start_column_index:end_column_index]
-		column_names[i] = column_name
-		i+=1
-		#print(column_name)
-	# In[ ]:
-	data_dic[table_names[table_name_index]]["columns"] = column_names
-	#print(data_dic)
-	table_name_index += 1
-
-print(data_dic)
+	print(data_dic)
